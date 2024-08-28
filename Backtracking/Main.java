d4709dbedf9c208e3e24d582de03dd23872243aa
Analia Veronica Burgos <112092368+Ayvero@users.noreply.github.com>
2024-08-28 19:19:19 -0300

    Update Main.java

--- Backtracking/Main.java
+++ Backtracking/Main.java
@@ -10,7 +10,7 @@ package Backtracking;
  * representar el problema mediante un grafo dirigido, donde cada nodo es una
  * habitaci�n, y cada puerta es un arco dirigido hacia otra habitaci�n.
  * 
- * @author Gentil Ricardo
+ * 
  *
  */
 public class Main {
